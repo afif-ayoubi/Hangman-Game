f8d96b7497f214114c52fdcfc302b99df8f035bf
afif-ayoubi <afifayoubi22@gmail.com>
2024-03-02 16:44:56 +0200

    writing and display functions

--- scripts/game.js
+++ scripts/game.js
@@ -1,4 +1,5 @@
 const answerSection = document.getElementById("answer-section");
+const letters = document.querySelector(".letters");
 const words = [
   "javascript",
   "python",
@@ -11,11 +12,51 @@ const words = [
   "kotlin",
   "typescript",
 ];
-const randomWord = pickRandomWord(words);
-const dahedWord=displaydashes(randomWord);
-function pickRandomWord(array) {
-  return array[Math.floor(Math.random() * array.length)];
+let randomWord = pickRandomWord();
+let dashedWord = displayDashes(randomWord);
+
+function pickRandomWord() {
+  return words[Math.floor(Math.random() * words.length)];
 }
-function displaydashes(word) {
+
+function displayDashes(word) {
   return "_".repeat(word.length);
 }
+
+function updateDashedWord(word, guessedLetter) {
+  let newDashedWord = "";
+  for (let i = 0; i < word.length; i++) {
+    if (word[i] === guessedLetter) {
+      newDashedWord += guessedLetter;
+    } else {
+      newDashedWord += dashedWord[i];
+    }
+  }
+  return newDashedWord;
+}
+
+function displayOnAnswerSection() {
+  answerSection.textContent = dashedWord.split("").join(" ");
+}
+
+letters.addEventListener("click", function (event) {
+  if (event.target.classList.contains("letter")) {
+    const selectedLetter = event.target.textContent.toLowerCase();
+    if (randomWord.includes(selectedLetter)) {
+      dashedWord = updateDashedWord(randomWord, selectedLetter);
+      displayOnAnswerSection();
+    }
+  }
+});
+
+document.addEventListener("keydown", function (event) {
+  const pressedKey = event.key.toLowerCase();
+  if (/[a-z]/.test(pressedKey)) {
+    if (randomWord.includes(pressedKey)) {
+      dashedWord = updateDashedWord(randomWord, pressedKey);
+      displayOnAnswerSection();
+    }
+  }
+});
+
+displayOnAnswerSection();
